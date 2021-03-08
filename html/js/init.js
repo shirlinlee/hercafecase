b6f1c1bd3e007050258ed59a4df1995a5c5bcf37
lee <shirlin@fofo.tw>
2021-03-09 01:11:39 +0800

    wip

--- html/js/init.js
+++ html/js/init.js
@@ -1,10 +1,14 @@
 $(function() {
-  console.log("123");
   $(".switch_btn").on("click", "p", function() {
     $(this).addClass("active");
     $(this)
       .siblings()
       .removeClass("active");
     $(".veiw").toggleClass("active");
+  });
+
+  $("body").on("click", ".nav>li", function() {
+    $(".nav>li").removeClass("active");
+    $(this).addClass("active");
   });
 });
